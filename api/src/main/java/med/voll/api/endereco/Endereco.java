d96746bfc764467ab4919f62bde71300d4ffb0c7
unknown <queren.fernandes@yahoo.com>
2023-11-08 17:27:43 -0300

    progresso

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,24 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Embeddable
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+}

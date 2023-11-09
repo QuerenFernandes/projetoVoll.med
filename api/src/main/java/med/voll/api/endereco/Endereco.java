a5a32e2889ec0dc4ace929491b81683762c179eb
unknown <queren.fernandes@yahoo.com>
2023-11-09 17:09:13 -0300

    integração com o banco ok

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -21,4 +21,14 @@ public class Endereco {
     private String cidade;
     private String uf;
 
+    //CONSTRUTOR DA CLASSE
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+    }
 }
